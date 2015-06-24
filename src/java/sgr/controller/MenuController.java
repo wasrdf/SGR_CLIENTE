77db2d9647dfabbfffd2c23693fd62a3db14f176
WASHINGTON <WASHINGTON@10.12.233.184>
2015-06-24 17:11:28 -0300

    Revert ""
    This reverts commit 987f624f753e8acf6a9b48199ac34b8063038a0b.

--- src/java/sgr/controller/MenuController.java
+++ src/java/sgr/controller/MenuController.java
@@ -73,7 +73,7 @@ public class MenuController {
         boolean itemStatus = false;
         System.out.println("STATUS DA CONTA:" + clientLogado.getSessionBean().isStatus());
         for (int i = 0; i < clientLogado.listaMovimento.size(); i++) {
-            if (!clientLogado.listaMovimento.get(i).getItemStatus().equals("Entregue")) {
+            if ((clientLogado.listaMovimento.get(i).getItemStatus().equals("Solicitado")) || (clientLogado.listaMovimento.get(i).getItemStatus().equals("Pronto"))) {
                 itemStatus = false;
             } else {
                 itemStatus = true;
@@ -104,8 +104,8 @@ public class MenuController {
     public void solicitarCancelamento(MovimentoBean pMovimento) {
         ContaItemService contaItemService = new ContaItemService();
 
-        if (!(pMovimento.getItemStatus().equals("Solicitado"))) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Você não pode cancelar o item pois o mesmo já foi entregue, ou se encontra em preparo.", ""));
+        if (pMovimento.getItemStatus().equals("Entregue")) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Você não pode cancelar o item pois o mesmo já foi entregue.", ""));
         } else {
 
             contaItemBean.setCodigo(pMovimento.getContaItemCodigo());
